ab127e67965b0d9ce2c5d1c883445b50168c5cb8
Bartek Bielawski <bartek.bielawski@live.com>
2018-04-24 21:25:51 +0200

    Adding first assertion + export tests + more clear test headers

--- tests/EWS.Tests.ps1
+++ tests/EWS.Tests.ps1
@@ -3,11 +3,19 @@ $prefix = 'Global tests of EWS module'
 Describe "$prefix - manifest" {
     Context 'Dot-sourced files' {
         foreach ($file in Get-ChildItem -Path .\EWS\*.ps1) {
-            It 'Should have name like PS function that it contains' {
+            $fileName = $file.Name
+            $baseName = $file.BaseName
+            $module = Get-Module -Name EWS
+            It "$fileName should have name like PS function that it contains" {
                 $file.Name | Should -Match ^\w+-EWS\w+\.ps1
             }
-            It 'Should have name that starts with approved verb' {
+            
+            It "$fileName should have name that starts with approved verb" {
                 $file.Name -replace '^(.*?)-.*', '$1' | Should -BeIn (Get-Verb).Verb
+            }
+            
+            It "$baseName should be exported as a command from the module" {
+                $module | Should -Export -CommandName $baseName 
             }
         }
     }
