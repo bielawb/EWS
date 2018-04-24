ada977d3ef5dd10f903fed2c057d5636cd5ac887
Bartek Bielawski <bartek.bielawski@live.com>
2018-04-24 21:32:33 +0200

    Attempt to upload test results

--- build/Test.ps1
+++ build/Test.ps1
@@ -1,4 +1,14 @@
 Import-Module -Name Pester
 Import-Module -Name .\EWS
 Import-Module -Name .\tests\Assertions.psm1
-Invoke-Pester -Script .\tests\ -EnableExit
+Invoke-Pester -Script .\tests\ -OutputFormat NUnitXml -OutputFile .\tests\results.xml
+try {
+    [System.Net.WebClient]::
+        new().
+        UploadFile(
+            "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", 
+            (Resolve-Path .\tests\results.xml)
+        )
+} catch {
+    throw "Failed to upload tests - $_"
+}
